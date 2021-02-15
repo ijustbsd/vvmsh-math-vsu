bb4d6e2f7c37cf6432cb1d66e89ddc93829695ba
Sergey Baboshin <ijustbsd@gmail.com>
2021-02-15 12:06:03 +0300

    Add participants list auto updates

--- exec.sh
+++ exec.sh
@@ -1,3 +1,10 @@
 #!bin/sh
+
+curl -L -o "data.csv" "https://docs.google.com/spreadsheets/d/${GDRIVE_DOCUMENT_ID}/export?format=csv&gid=${GDRIVE_SHEET_ID}"
+
+python3 formatter.py
+
+members=$(cat members.json)
+
 cd /pelican
 pelican -d content
